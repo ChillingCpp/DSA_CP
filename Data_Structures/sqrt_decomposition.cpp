911a5f48732145755fc637a46147643218a26699
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-07-26 11:02:36 +0700

    Update sqrt_decomposition.cpp

--- Data_Structures/sqrt_decomposition.cpp
+++ Data_Structures/sqrt_decomposition.cpp
@@ -39,61 +39,39 @@ struct Mo
 
         inline ll hilbertOrder(int x, int y, int pow)
         {
-            ll d = 0;
-            for (int s = pow - 1; s >= 0; --s)
+            // true hilbert, in right order so faster than Z order
+            ll  res = 0;
+            int rot = 0;
+
+            for (int p = pow; p > 0; --p)
             {
-                int rx = (x >> s) & 1;
-                int ry = (y >> s) & 1;
-                d <<= 2;
-                d |= (rx << 1) | ry;
-                if (ry == 0)
+                int hp  = 1 << (p - 1);
+                int seg = (x < hp ? 1 : 0) ^ (y < hp ? 1 : 2);
+                
+                static const int rotateDelta[4] = { 3, 0, 0, 1 };
+
+                int nrot = (rot + rotateDelta[seg]) & 3;
+
+                int nx = x & (hp - 1), ny = y & (hp - 1);
+                res += seg * (1ll << (2 * p - 2));
+                rot = nrot;
+                switch (seg)
                 {
-                    if (rx == 1)
-                    {
-                        x = (1 << pow) - 1 - x;
-                        y = (1 << pow) - 1 - y;
-                    }
-                    std::swap(x, y);
+                    case 0:
+                        x = ny;
+                        y = nx;
+                    break;
+                    case 3:
+                        x = hp - ny - 1;
+                        y = hp - nx - 1;
+                    break;
+                    default:
+                        x = nx;
+                        y = ny;
+                    break;
                 }
             }
-
-            return d;
-
-        // true hilbert, in right order so faster than Z order
-        ll  res = 0;
-        int rot = 0;
-
-        for (int p = pow; p > 0; --p)
-        {
-            int hp  = 1 << (p - 1);
-            int seg = (x < hp ? 1 : 0) ^ (y < hp ? 1 : 2);
-
-
-            static const int rotateDelta[4] = { 3, 0, 0, 1 };
-
-            int nrot = (rot + rotateDelta[seg]) & 3;
-
-            int nx = x & (hp - 1), ny = y & (hp - 1);
-            res += seg * (1ll << (2 * p - 2));
-            rot = nrot;
-            switch (seg)
-            {
-                case 0:
-                    x = ny;
-                    y = nx;
-                break;
-                case 3:
-                    x = hp - ny - 1;
-                    y = hp - nx - 1;
-                break;
-                default:
-                    x = nx;
-                    y = ny;
-                break;
-            }
-        }
-
-        return res;
+            return res;
         }
         bool operator<(const Query& b)
         {
