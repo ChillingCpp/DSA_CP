48a3d405ab22c8354bd0fc32d3665ec3abdc48ef
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-07-26 10:58:03 +0700

    Update sqrt_decomposition.cpp

--- Data_Structures/sqrt_decomposition.cpp
+++ Data_Structures/sqrt_decomposition.cpp
@@ -59,20 +59,41 @@ struct Mo
 
             return d;
 
-            //// recursion
-            // if (pow == 0)
-            //     return 0;
-            // int hpow                        = 1 << (pow - 1);
-            // int seg                         = (x < hpow ? 0 : 1) | (y < hpow ? 0 : 2);
-            // seg                             = (seg + rot) & 3;
-            // static const int rotateDelta[4] = { 3, 0, 0, 1 };
-            // int              nx = x & (hpow - 1), ny = y & (hpow - 1);
-            // int              nrot          = (rot + rotateDelta[seg]) & 3;
-            // ll               subSquareSize = 1LL << (2 * pow - 2);
-            // ll               ans           = seg * subSquareSize;
-            // ll               add           = hilbertOrder(nx, ny, pow - 1, nrot);
-            // ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
-            // return ans;
+    // true hilbert, in right order so faster than Z order
+    ll  res = 0;
+    int rot = 0;
+
+    for (int p = pow; p > 0; --p)
+    {
+        int hp  = 1 << (p - 1);
+        int seg = (x < hp ? 1 : 0) ^ (y < hp ? 1 : 2);
+
+
+        static const int rotateDelta[4] = { 3, 0, 0, 1 };
+
+        int nrot = (rot + rotateDelta[seg]) & 3;
+
+        int nx = x & (hp - 1), ny = y & (hp - 1);
+        res += seg * (1ll << (2 * p - 2));
+        rot = nrot;
+        switch (seg)
+        {
+            case 0:
+                x = ny;
+                y = nx;
+                break;
+            case 3:
+                x = hp - ny - 1;
+                y = hp - nx - 1;
+                break;
+            default:
+                x = nx;
+                y = ny;
+                break;
+        }
+    }
+
+    return res;
         }
         bool operator<(const Query& b)
         {
