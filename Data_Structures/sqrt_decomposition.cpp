061e2bbd67599a46a313b8c032ca4bd85dfba8fe
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-07-26 10:58:42 +0700

    Update sqrt_decomposition.cpp

--- Data_Structures/sqrt_decomposition.cpp
+++ Data_Structures/sqrt_decomposition.cpp
@@ -59,41 +59,41 @@ struct Mo
 
             return d;
 
-    // true hilbert, in right order so faster than Z order
-    ll  res = 0;
-    int rot = 0;
+        // true hilbert, in right order so faster than Z order
+        ll  res = 0;
+        int rot = 0;
 
-    for (int p = pow; p > 0; --p)
-    {
-        int hp  = 1 << (p - 1);
-        int seg = (x < hp ? 1 : 0) ^ (y < hp ? 1 : 2);
-
-
-        static const int rotateDelta[4] = { 3, 0, 0, 1 };
-
-        int nrot = (rot + rotateDelta[seg]) & 3;
-
-        int nx = x & (hp - 1), ny = y & (hp - 1);
-        res += seg * (1ll << (2 * p - 2));
-        rot = nrot;
-        switch (seg)
+        for (int p = pow; p > 0; --p)
         {
-            case 0:
-                x = ny;
-                y = nx;
-                break;
-            case 3:
-                x = hp - ny - 1;
-                y = hp - nx - 1;
-                break;
-            default:
-                x = nx;
-                y = ny;
-                break;
-        }
-    }
+            int hp  = 1 << (p - 1);
+            int seg = (x < hp ? 1 : 0) ^ (y < hp ? 1 : 2);
 
-    return res;
+
+            static const int rotateDelta[4] = { 3, 0, 0, 1 };
+
+            int nrot = (rot + rotateDelta[seg]) & 3;
+
+            int nx = x & (hp - 1), ny = y & (hp - 1);
+            res += seg * (1ll << (2 * p - 2));
+            rot = nrot;
+            switch (seg)
+            {
+                case 0:
+                    x = ny;
+                    y = nx;
+                break;
+                case 3:
+                    x = hp - ny - 1;
+                    y = hp - nx - 1;
+                break;
+                default:
+                    x = nx;
+                    y = ny;
+                break;
+            }
+        }
+
+        return res;
         }
         bool operator<(const Query& b)
         {
