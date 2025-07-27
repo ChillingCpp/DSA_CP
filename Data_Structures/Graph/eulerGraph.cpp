1582039fa9cd4b3a0787696482cc5e6a065567cc
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-07-27 22:09:24 +0700

    Update and rename euler.cpp to eulerGraph.cpp

---
+++ Data_Structures/Graph/eulerGraph.cpp
@@ -0,0 +1,49 @@
+
+vector<vector<int>> a;
+vector<int>         in, out;
+vector<int>         paths;
+
+void dfs(int u)
+{
+    while (out[u])
+    {
+        for (auto& v : a[u])
+            dfs(--out[v]);
+    }
+    paths.push_back(u);
+}
+void euler()
+{
+    int n = a.size();
+    for (int i = 0; i < n; ++i)
+    {
+        for (auto j : a[i])
+        {
+            out[i]++;
+            in[j]++;
+        }
+    }
+    bool c1 = false, c2 = false, no = false;
+    int  start = 1;
+    for (int i = 0; i < n; ++i)
+    {
+        if (out[i] - in[i] > 1 || in[i] - out[i] > 1)
+            no = true;
+        if (out[i] - in[i] == 1)
+        {
+            if (c1)
+                no = true;
+            c1    = true;
+            start = i;
+        }
+        if (in[i] - out[i] == 1)
+        {
+            if (c2)
+                no = true;
+            c2 = true;
+        }
+    }
+    if (no)
+        return;
+    dfs(start);
+}
