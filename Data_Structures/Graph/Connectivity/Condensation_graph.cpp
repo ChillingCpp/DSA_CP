1b54b3ae09f96b1e9a90ea3d9c7c3555be6dae5a
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-05 15:28:55 +0700

    Update Condensation_graph.cpp

--- Data_Structures/Graph/Connectivity/Condensation_graph.cpp
+++ Data_Structures/Graph/Connectivity/Condensation_graph.cpp
@@ -1,1 +1,6 @@
+// we decompose a graph with cycles to a condensation graph
+// A condensation graph is a Directed acyclic graph connect strongly connected componenents, which mean its a graph which Sccs are the vertices.
+// condensation graph useful when we are dealing some representative value of a scc (mincost/maxcost travel)
+// combine condensation graph with topo sort and dp to achive solution in O(N + M) ( topo sort is a dp technique )
 
+// first get all sccs then connect sccs together, using tarjan algorithm
