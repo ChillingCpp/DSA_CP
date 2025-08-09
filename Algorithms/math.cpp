a207f8f261b714ccb7cff21da8c9ec7a86c43a59
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-09 18:08:34 +0700

    Update math.cpp

--- Algorithms/math.cpp
+++ Algorithms/math.cpp
@@ -1,5 +1,32 @@
 
+// some math function for cp
+ll binmod(ll a, ll b, ll mod)
+{
+    a %= mod;
+    ll res = 1;
+    while (b){
+        if (b & 1) res = (res * a) % mod;
+        a = (a * a) % mod;
+    }
+}
 ll nCk(ll n, ll k)
 {
     k = min(k, n - k);
+    ll res = 1;
+    for (ll i = 1; i <= k; ++i)
+    {
+        res = res * (n - i + 1) / i;
+    }
+    return res;
+}
+
+ll nCk(ll n, ll k, ll mod)
+{
+    k = min(k, n - k);
+    ll res = 1;
+    for (ll i = 1; i <= k; ++i)
+    {
+        res = res * (n - i + 1) / i;
+    }
+    return res;
 }
