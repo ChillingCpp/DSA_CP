af22c53d69fe12085b10aa022ca87012faa68dbc
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-10 12:52:37 +0700

    Update DP_Topological_sort.cpp

--- Data_Structures/Graph/DP_Topological_sort.cpp
+++ Data_Structures/Graph/DP_Topological_sort.cpp
@@ -1,1 +1,32 @@
+int n = 1e5;
+vector<int> mode(n+1, 0); // 0 : unvisted, 1: visited and still in dfs tree, 2 : completed visited, will never untouched.
+vector<vector<int>> a;
+vector<int> ans;
+bool dfs(int u)
+{
+    mode[u] = 1;
+    for (auto& v: a[u]){
+        if (mode[v] == 1) 
+            return false;
+        if (!mode[v] && !dfs(v))
+            return false;
+    }
+    mode[u] = 2;
+    ans.push_back(u);
+    return true;
+}
 
+// how to use : //
+bool cycle = 0;
+for (int i = 1; i <= n; ++i)
+    if (!mode[i])
+        if (!dfs(i)){
+            cycle = 1;
+            break;
+        }
+}
+if (cycle){
+    cout << "Cycle detected, abort\n":
+    return 0;
+}
+reverse(ans.begin(), ans.end());
