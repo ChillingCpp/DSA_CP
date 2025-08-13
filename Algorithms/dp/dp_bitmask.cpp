026ecdfa60870792d36ffb0c639fb7f48dac10b5
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-13 22:41:14 +0700

    Create dp_bitmask.cpp

---
+++ Algorithms/dp/dp_bitmask.cpp
@@ -0,0 +1,27 @@
+    int tsp(vector<vector<int>>& dist) {
+        // Code here
+        int n = dist.size();
+        int inf = 1e9;
+        vector<vector<int>> dp(1 << n, vector<int>(n, inf));
+        
+        dp[1][0] = 0;
+        for (int mask = 0; mask < (1 << n); ++mask)
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                if (!(mask & (1 << i))) continue; //  check i is in setbit
+                for (int j = 0; j < n; ++j)
+                {
+                    
+                    if (mask & (1 << j)) continue; // transition from i to j require j not in the mask
+                    int newmask = mask | (1 << j);
+                        dp[newmask][j] = min(dp[newmask][j], dp[mask][i] + dist[i][j]);
+                }
+            }
+        }
+        int ans = inf;
+        for (int i = 0; i < n; ++i)
+            ans = min(ans, dp[(1 << n) -1][i] + dist[i][0]);
+
+        return ans;
+    }
