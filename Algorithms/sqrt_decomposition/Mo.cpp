bd351a78d1f67ca15eb9e46840b496237513a4a5
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-16 12:34:14 +0700

    update sqrt_decomposition

---
+++ Algorithms/sqrt_decomposition/Mo.cpp
@@ -0,0 +1,245 @@
+// Mo algorithms, a technique using sqrt decomposition
+struct Mo
+{
+    ll          res = 0;
+    vector<int> a, b;
+    struct Query
+    {
+        int l, r, idx;
+        ll  ord;
+
+        Query(int l_, int r_, int idx_) : l(l_), r(r_), idx(idx_)
+        {
+            ord = hilbert_order(l, r, 20);
+        }
+        inline ll hilbert_order(int x, int y, int pow)
+        {
+            ll  res = 0;
+            int rot = 0;
+
+            for (int p = pow; p > 0; --p)
+            {
+                int hp  = 1 << (p - 1);
+                int seg = (x < hp ? 1 : 0) ^ (y < hp ? 1 : 2);
+                
+                static const int rotateDelta[4] = { 3, 0, 0, 1 };
+
+                rot = (rot + rotateDelta[seg]) & 3;
+
+                int nx = x & (hp - 1), ny = y & (hp - 1);
+                res += seg * (1ll << (2 * p - 2));
+                switch (seg)
+                {
+                    case 0:
+                        x = ny;
+                        y = nx;
+                    break;
+                    case 3:
+                        x = hp - ny - 1;
+                        y = hp - nx - 1;
+                    break;
+                    default:
+                        x = nx;
+                        y = ny;
+                    break;
+                }
+            }
+            return res;
+        }
+        bool operator<(const Query& b)
+        {
+            return ord < b.ord;
+        }
+    };
+
+    void add(int idx){
+        /// adding an element to current range, O(1) or O(log n)
+        res += a[idx];
+    }
+    void remove(int idx){
+        /// remove an element from current range, O(1) or O(log n)
+        res -= a[idx];
+    }
+    void solve()
+    {
+        //// ..... ///// tiền xử lí
+
+
+        /// lưu truy vấn
+        vector<Query> queries(q);
+        for (int i = 0; i < q; ++i)
+        {
+            int l, r;
+            cin >> l >> r;
+            queries[i] = { l - 1, r - 1, i };  // convert to 0-based
+        }
+
+        sort(queries.begin(), queries.end());
+
+        vector<ll> ans(q);
+        
+        int L = 0, R = -1;
+        for (const auto& qr : queries)
+        {
+            int l = qr.l;
+            int r = qr.r;
+
+            while (R < r) add(++R);
+            while (L > l) add(--L);
+            while (R > r) remove(R--);
+            while (L < l) remove(L++);
+            ans[qr.idx] = res;
+        }
+
+        for (ll x : ans)
+            cout << x << '\n';
+    }
+};
+
+
+// Mo algorithms, a technique using sqrt decomposition
+struct MoUpdate
+{
+    ll          res = 0;
+    vector<int> a, b;
+    struct Query
+    {
+        int l, r, idx, t;
+        ll  ord;
+
+        Query(int l_, int r_, int idx_, int _t)
+        : l(l_)
+        , r(r_)
+        , idx(idx_)
+        , t(_t)
+        {
+            ord = hilbert_order(l, r, 20);
+        }
+        inline ll hilbert_order(int x, int y, int pow)
+        {
+            ll  res = 0;
+            int rot = 0;
+
+            for (int p = pow; p > 0; --p)
+            {
+                int hp  = 1 << (p - 1);
+                int seg = (x < hp ? 1 : 0) ^ (y < hp ? 1 : 2);
+
+                static const int rotateDelta[4] = { 3, 0, 0, 1 };
+
+                rot = (rot + rotateDelta[seg]) & 3;
+
+                int nx = x & (hp - 1), ny = y & (hp - 1);
+                res += seg * (1ll << (2 * p - 2));
+                switch (seg)
+                {
+                    case 0:
+                        x = ny;
+                        y = nx;
+                        break;
+                    case 3:
+                        x = hp - ny - 1;
+                        y = hp - nx - 1;
+                        break;
+                    default:
+                        x = nx;
+                        y = ny;
+                        break;
+                }
+            }
+            return res;
+        }
+        bool operator<(const Query& b)
+        {
+            return ord < b.ord;
+        }
+    };
+    struct Update
+    {
+        int idx;
+        ll  old;
+        ll  val;
+    };
+
+    void add(int idx)
+    {
+        /// adding an element to current range, O(1) or O(log n)
+        res += a[idx];
+    }
+    void remove(int idx)
+    {
+        /// remove an element from current range, O(1) or O(log n)
+        res -= a[idx];
+    }
+
+    void solve()
+    {
+        //// ..... ///// tiền xử lí
+        int n, q;
+        cin >> n >> q;
+        a.resize(n);
+        cin >> a;
+        b = a;
+
+        /// lưu truy vấn
+        vector<Query>  queries;
+        vector<Update> updates;
+        for (int i = 0; i < q; ++i)
+        {
+            int ty, u, v;
+            cin >> ty >> u >> v;
+            if (ty == 1)
+            {
+                updates.push_back({ u, b[u], v });
+                b[u] = v;
+            }
+            else
+            {
+                // if 1 based then -1
+                queries.push_back({ u - 1, v - 1, i, (int) updates.size() - 1 });
+            }
+        }
+
+        sort(queries.begin(), queries.end());
+
+        vector<ll> ans(q);
+
+        int  L = 0, R = -1;
+        auto update = [&](int i, int x)
+        {
+            if (L <= i && i <= R)
+            {
+                res -= a[i];
+                res += x;
+            }
+            a[i] = x;
+        };
+        int t = -1;
+        for (const auto& qr : queries)
+        {
+            int l = qr.l;
+            int r = qr.r;
+
+            while (t < qr.t)
+                t++, update(updates[t].idx, updates[t].val);
+            while (t > qr.t)
+                update(updates[t].idx, updates[t].old), t--;
+
+            while (R < r)
+                add(++R);
+            while (L > l)
+                add(--L);
+            while (R > r)
+                remove(R--);
+            while (L < l)
+                remove(L++);
+            ans[qr.idx] = res;
+        }
+
+        for (ll x : ans)
+            cout << x << '\n';
+    }
+};
+
+/// Mo on tree or graph require some thing to turn these queries into a flat array, making range queries useful
+
---
+++ Algorithms/sqrt_decomposition/block_decomposition.cpp
@@ -0,0 +1,179 @@
+
+/// ý tưởng : chia mảng A thành ceil(sqrt(n)) mảng có độ dài sqrt(n)
+
+//// có kết hợp kĩ thuật lazy propagation tương tự segment tree (apply lazy vào các block
+
+struct SqrtDecomposition{
+
+    int n;
+    int S;
+    vector<ll> b;
+    vector<ll> a;
+    SqrtDecomposition(vector<ll> &v) : n(v.size()), a(v)
+    {
+        S = (int) sqrt(n) + 1;
+        b.resize(S);
+        for (int i = 0; i < n; ++i)
+            b[i / S] += a[i];
+    }
+    
+    /// range query - Immutable
+    ll query(int l, int r){
+
+        int c_l = l / S, c_r = r / S;
+        ll sum = 0;
+        if (c_l == c_r) for (int i = l; i <= r; ++i) sum += a[i];
+        else
+        { 
+            for (int i = l; i < (c_l + 1) * S; ++i) sum += a[i]; /// [l....last_c_l]
+            for (int i = c_r*S; i <= r; ++i) sum += a[i]; // [first....r]
+            for (int i = c_l + 1; i < c_r; ++i) sum += b[i]; // calculate the block fit in range
+        }
+        return sum;
+    }
+};
+
+
+// range query - range update
+struct SqrtLazy
+{
+    int n;
+    int S;
+    vector<ll> b;
+    vector<ll> a;
+    vector<ll> lazy;
+    SqrtDecomposition(vector<ll> &v) : n(v.size()), a(v)
+    {
+        S = (int) sqrt(n) + 1;
+        b.resize(S);
+        lazy.resize(S);
+        for (int i = 0; i < n; ++i)
+            b[i / S] += a[i];
+    }
+    
+    void apply(int bid)
+    {
+        int l = bid * S;
+        int r = (bid+1)* S -1;
+    }
+    ll update(int l, int r, ll val)
+    {
+        int c_l = l / S;
+        int c_r = r / S;
+        if (c_l == c_r)
+        {
+            apply(c_l);
+        }
+    }
+    /// range query - Immutable
+    ll query(int l, int r){
+
+        int c_l = l / S, c_r = r / S;
+        ll sum = 0;
+        if (c_l == c_r) for (int i = l; i <= r; ++i) sum += a[i];
+        else
+        { 
+            for (int i = l; i < (c_l + 1) * S; ++i) sum += a[i]; /// [l....last_c_l]
+            for (int i = c_r*S; i <= r; ++i) sum += a[i]; // [first....r]
+            for (int i = c_l + 1; i < c_r; ++i) sum += b[i]; // calculate the block fit in range
+        }
+        return sum;
+    }
+}
+
+// range query - range update
+struct SqrtLazy
+{
+    int        n;
+    int        S;
+    vector<ll> b;
+    vector<ll> a;
+    vector<ll> lazy;
+    SqrtLazy(vector<ll>& v)
+    : n(v.size())
+    , a(v)
+    {
+        S = (int) sqrt(n) + 1;
+        b.resize(S);
+        lazy.resize(S);
+        for (int i = 0; i < n; ++i)
+            b[i / S] += a[i];
+    }
+    int blocksize(int i)
+    {
+        int l = i * S;
+        int r = min(n - 1, (i + 1) * S - 1);
+        return r - l + 1;
+    }
+    void apply(int bid)
+    {
+        b[bid] = 0;
+        int l  = bid * S;
+        int r  = min(n - 1, (bid + 1) * S - 1);
+        for (int i = l; i <= r; ++i)
+            a[i] += lazy[bid];
+
+        for (int i = l; i <= r; ++i)
+            b[bid] += a[i];
+
+        lazy[bid] = 0;
+    }
+    void addlazy(int c_l, int c_r, ll val)
+    {
+        for (int i = c_l; i <= c_r; ++i)
+            lazy[i] += val;
+    }
+    void manual_update(int l, int r, ll val)
+    {
+        int c = l / S;
+        for (int i = l; i <= r; ++i)
+        {
+            a[i] += val;
+            b[c] += val;
+        }
+    }
+    void update(int l, int r, ll val)
+    {
+        int c_l = l / S;
+        int c_r = r / S;
+        if (c_l == c_r)
+        {
+            apply(c_l);
+            manual_update(l, r, val);
+        }
+        else
+        {
+            apply(c_l);
+            apply(c_r);
+
+            addlazy(c_l + 1, c_r - 1, val);
+
+            manual_update(l, min(n - 1, (c_l + 1) * S - 1), val);
+            manual_update(c_r * S, r, val);
+        }
+    }
+    /// range query - Immutable
+    ll query(int l, int r)
+    {
+        int c_l = l / S, c_r = r / S;
+        ll  sum = 0;
+        if (c_l == c_r)
+        {
+            apply(c_l);
+            for (int i = l; i <= r; ++i)
+                sum += a[i];
+        }
+        else
+        {
+            apply(c_l);
+            apply(c_r);
+            for (int i = l; i < (c_l + 1) * S; ++i)
+                sum += a[i];  /// [l....last_c_l]
+            for (int i = c_r * S; i <= r; ++i)
+                sum += a[i];  // [first....r]
+            for (int i = c_l + 1; i < c_r; ++i)
+                sum += (b[i] + lazy[i] * blocksize(i));  // calculate the block fit in range
+        }
+        return sum;
+    }
+};
