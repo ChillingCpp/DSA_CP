72c047555c0d0f8119a4b841469f5e7d3a2e208e
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-18 10:44:04 +0700

    Update eulerTour.cpp

--- range_query/GraphTree/eulerTour.cpp
+++ range_query/GraphTree/eulerTour.cpp
@@ -1,14 +1,12 @@
-vector<bool>        vis;
 vector<int>         paths;  // paths.reserve(2 * n);
 vector<vector<int>> a;
 
-void dfs(int u)
+void dfs(int u, int p)
 {
-    vis[u]       = true;
     paths.push_back(u);
     for (int& v : a[u])
     {
-        if (vis[v])
+        if (v == p)
             continue;
         dfs(v);
         paths.push_back(u);
