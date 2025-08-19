d27c2dfa48adf0ac77b1bf0773757112c69dcd97
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-19 19:23:39 +0700

    Update Mo.cpp

--- range_query/sqrt_decomposition/MO_algorithm/Mo.cpp
+++ range_query/sqrt_decomposition/MO_algorithm/Mo.cpp
@@ -1,7 +1,3 @@
-ll hilbert_order(int x, int y, int pow)
-{
-    ////// ..... /////// see hilbert_order.cpp for more information
-}
 
 // Mo algorithms, a technique using sqrt decomposition
 struct Mo
@@ -17,7 +13,10 @@ struct Mo
         {
             ord = hilbert_order(l, r, 20);
         }
-
+        inline ll hilbert_order(int x, int y, int pow)
+        {
+            ////// ..... /////// see hilbert_order.cpp for more information
+        }
         bool operator<(const Query& b)
         {
             return ord < b.ord;
