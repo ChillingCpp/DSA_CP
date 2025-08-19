2049b6c423f6273b7806f9646032d833938fb630
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-19 19:22:36 +0700

    Update hilbert_order.cpp

--- range_query/sqrt_decomposition/MO_algorithm/hilbert_order.cpp
+++ range_query/sqrt_decomposition/MO_algorithm/hilbert_order.cpp
@@ -2,17 +2,18 @@ ll hilbert_order(int x, int y, int pow)
 {
     ll  res = 0;
     int rot = 0;
+    static const int rotdel[4] = { 3, 0, 0, 1 };
 
     for (int p = pow; p > 0; --p)
     {
         int hp  = 1 << (p - 1);
+        
         int seg = (x < hp ? 1 : 0) ^ (y < hp ? 1 : 2);
-
-        static const int rotateDelta[4] = { 3, 0, 0, 1 };
-
-        rot = (rot + rotateDelta[seg]) & 3;
+        
+        rot = (rot + rotdel[seg]) & 3;
 
         int nx = x & (hp - 1), ny = y & (hp - 1);
+        
         res += seg * (1ll << (2 * p - 2));
         switch (seg)
         {
@@ -31,4 +32,5 @@ ll hilbert_order(int x, int y, int pow)
         }
     }
     return res;
+
 }
