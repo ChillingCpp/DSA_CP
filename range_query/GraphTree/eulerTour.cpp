1a0927cc1ccd386090d23d902afae0ec842a61b3
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-20 21:07:00 +0700

    Update eulerTour.cpp

--- range_query/GraphTree/eulerTour.cpp
+++ range_query/GraphTree/eulerTour.cpp
@@ -2,6 +2,7 @@ vector<int>         euler, st, en;  // paths.reserve(2 * n);
 vector<vector<int>> a;
 int timer = 0; // if 1 based
 
+/// Mo algorithms or subtree queries
 void dfs(int u, int p)
 {
     st[u]        = ++timer;
@@ -16,4 +17,23 @@ void dfs(int u, int p)
 
     en[u]        = ++timer;
     euler[timer] = u;
+}
+
+
+// for dynamic tree, support add/remove, online queries
+vector<int>         paths;  // paths.reserve(2 * n);
+vector<vector<int>> a;
+
+
+void dfs(int u, int p)
+{
+    paths.push_back(u);
+    for (int& v : a[u])
+
+    {
+        if (v == p)
+            continue;
+        dfs(v);
+        paths.push_back(u);
+    }
 }
