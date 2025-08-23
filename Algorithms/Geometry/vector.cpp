61cd6b686edb15bae68656df79097ffa29e4f6d2
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-23 09:54:48 +0700

    Update vector.cpp

--- Algorithms/Geometry/vector.cpp
+++ Algorithms/Geometry/vector.cpp
@@ -1,1 +1,40 @@
 
+struct Point
+{
+    ll x, y;
+};
+struct Vector
+{
+    ll x, y;
+    Vector() = default;
+    Vector(Point& a, Point& b) : x(b.x - a.x), y(b.y - a.y) {} // construct vector ab
+
+    ll dot(Vector& b)
+    {
+        return x * b.x + y * b.y;
+    }
+    ll cross(Vector& b)
+    {
+        return x * b.y - y * b.x;
+    }
+};
+
+bool isCW(Point& a, Point& b, Point& c)
+{
+    Vector ab(a, b);
+    Vector ac(a, c);
+    return ab.cross(ac) < 0;
+}
+bool isCCW(Point& a, Point& b, Point& c)
+{
+    Vector ab(a, b);
+    Vector ac(a, c);
+    return ab.cross(ac) > 0;
+}
+bool isCollinear(Point& a, Point& b, Point& c)
+{
+    Vector ab(a, b);
+    Vector ac(a, c);
+    return ab.cross(ac) == 0;
+}
+
