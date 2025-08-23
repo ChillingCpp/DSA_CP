f6a70725b87a6b464808c3af1befb35258f044e7
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-23 18:20:22 +0700

    Update Line.cpp

--- Algorithms/Geometry/Line.cpp
+++ Algorithms/Geometry/Line.cpp
@@ -1,1 +1,29 @@
 
+struct Line  // ax + by = c
+{
+    double a, b, c;
+    Line() = default;
+    Line(double a, double b, double c)
+    : a(a)
+    , b(b)
+    , c(c)
+    {
+    }
+    Line(const Point& A, const Point& B)  // convert vector u(a, b) to vector n(b, -a)
+    : a(B.y - A.y)
+    , b(A.x - B.x)
+    {
+        c = a * A.x + b * A.y;
+    }
+    bool contain(const Point& p)
+    {
+        return a * p.x + b * p.y - c == 0;
+    }
+    Line getprep(const Point& p)
+    {
+        double na = b;
+        double nb = -a;
+        double nc = na * p.x + nb * p.y;
+        return Line(na, nb, nc);
+    }
+}
