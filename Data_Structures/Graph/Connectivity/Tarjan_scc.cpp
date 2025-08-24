8b73b60890469854f2ff2b17dacefc604e02f443
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-24 10:56:29 +0700

    Update Tarjan_scc.cpp

--- Data_Structures/Graph/Connectivity/Tarjan_scc.cpp
+++ Data_Structures/Graph/Connectivity/Tarjan_scc.cpp
@@ -21,9 +21,15 @@ void tarjan(int u)
         if (!node[v].disc)
         {
             tarjan(v);
+            /// low[u] = min(low[u], low[v]) khi v chưa được khám phá là do ta có đường đi từ u -> v, 
+            /// mà ta lại biết v có thể quay về 1 tổ tiên có disc nhỏ nhất ( gọi là z ), 
+            /// vậy nên u cũng có thể quay về tổ tiên z đó ( theo u -> v -> z )
             node[u].low = min(node[u].low, node[v].low);
         }
         else if (node[v].st)
+            /// low[u] = min(low[u], disc[v]) khi v đã khám phá là do v là tổ tiên của u, xét hiện tại thì ta chỉ biết u có thể quay về v
+            /// ta sẽ không tính low[u] = min(low[u], low[v]) vì low[u] sẽ chỉ tính các back edge quay ngược trực tiếp 
+            /// chứ không phải qua 2 lần chạy back edge hoặc nhiều hơn từ u -> v -> z, với z là đỉnh có disc[z] = low[v], v có back edge khác ta sẽ không xét
             node[u].low = min(node[u].low, node[v].disc);
     }
 
