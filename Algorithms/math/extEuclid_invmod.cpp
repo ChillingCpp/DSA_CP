4a6ceb2d2ef55f2af39d4be4c2084b4c3e74b689
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-24 16:02:18 +0700

    Update extEuclid_invmod.cpp

--- Algorithms/math/extEuclid_invmod.cpp
+++ Algorithms/math/extEuclid_invmod.cpp
@@ -23,39 +23,6 @@ ll invmod(ll a, ll b, ll m){
     return x;
 }
 
-// a*x + b*y = c
-bool solveequ(ll a, ll b, ll c, ll&x0, ll& y0)
-{
-    ll g = euclid(abs(a), abs(b), x0, y0);
-    if (c % g) return false;
-
-    x0 *= (c / g);
-    y0 *= (c / g);
-    return true;
-}
-// y = ax + b;
-struct linear
-{
-    ll a = 0, b =0 ;
-};
-bool solveequ(ll a, ll b, ll c, ll&x0, ll& y0, linear& s1, linear& s2)
-{
-    ll g = euclid(abs(a), abs(b), x0, y0);
-    if (c % g) return false;
-
-    a /= g;
-    b /= g;
-    c /= g;
-    if (a < 0) x0 = -x0;
-    if (b < 0) y0 = -y0;
-    s1.a = b;
-    s1.b = x0 * c;
-    
-    s2.a = -a;
-    s2.b = y0 * c;
-    return true;
-}
-
 vector<ll> invmod(vector<ll> a, ll b, ll m)
 {
     ll n = a.size();
@@ -73,3 +40,4 @@ vector<ll> invmod(vector<ll> a, ll b, ll m)
     return inv;
 
 }
+
