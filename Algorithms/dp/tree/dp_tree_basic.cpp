37812710474d4d4f76b886b722e7cc6f1b1d30ae
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-28 22:20:10 +0700

    Update dp_tree_basic.cpp

--- Algorithms/dp/tree/dp_tree_basic.cpp
+++ Algorithms/dp/tree/dp_tree_basic.cpp
@@ -1,1 +1,14 @@
 
+vector<int> sz; 
+vector<vector<int>> a;
+
+void dfs(int u, int p)
+{
+    sz[u] = 1;
+    for (int v : a[u])
+    {
+        if (v == p) continue;
+        dfs(v, u);
+        sz[u] += sz[v]; /// include node v to u
+    }
+}
