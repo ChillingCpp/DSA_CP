9364828e5ac0e620a7dd2f2c09d8f510876aad34
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-08-29 21:42:39 +0700

    Update 3typesEulerTour.cpp

--- range_query/GraphTree/eulerTour.cpp
+++ range_query/GraphTree/eulerTour.cpp
@@ -1,11 +1,52 @@
-vector<int>         euler, st, en;  // paths.reserve(2 * n);
+// for dynamic tree, support add/remove, online queries, lca queries + segment tree, work with
+// segment tree lazy propagation.
+vector<int>         path, tin, tout;  // paths.reserve(2 * n - 1);, tin.resize(n), tout.resize(n);
 vector<vector<int>> a;
-int timer = 0; // if 1 based
 
-/// Mo algorithms or subtree queries
 void dfs(int u, int p)
 {
-    st[u]        = ++timer;
+    path.push_back(u);
+    tin[u] = path.size() - 1;
+    for (int& v : a[u])
+
+    {
+        if (v == p)
+            continue;
+        dfs(v, u);
+        path.push_back(u);
+    }
+    tout[u] = path.size() - 1;
+}
+
+/// dp on tree euler, if dp direction is from the parent to child (some dp require child to parent)
+/// then euler indexing is not workign, this version from usaco, best for subtree queries because
+/// [in[u], out[u]-1] is the subtree of u
+vector<int>         tin, tout, order;  /// tin.resize(n), tout.resize(n), order.resize(n);
+vector<vector<int>> a;
+int                 timer = 0;
+void                dfs(int u, int p)
+{
+    tin[u]        = timer++;
+    order[tin[u]] = u;  // pre-order order
+    for (int v : a[u])
+    {
+        if (v == p)
+            continue;
+        dfs(v, u);
+    }
+    tout[u] = timer;
+}
+
+
+vector<int>         euler, tin, tout;  // paths.reserve(2 * n);
+vector<vector<int>> a;
+int                 timer = 0;  // if 1 based
+
+/// Mo algorithms ( not for dp on tree )
+/// this version not support lca, need a binary lifting or the 2*n - 1 euler tour
+void dfs(int u, int p)
+{
+    tin[u]       = ++timer;
     euler[timer] = u;
     for (int v : a[u])
     {
@@ -15,25 +56,6 @@ void dfs(int u, int p)
         dfs(v, u);
     }
 
-    en[u]        = ++timer;
+    tout[u]      = ++timer;
     euler[timer] = u;
-}
-
-
-// for dynamic tree, support add/remove, online queries
-vector<int>         paths;  // paths.reserve(2 * n);
-vector<vector<int>> a;
-
-
-void dfs(int u, int p)
-{
-    paths.push_back(u);
-    for (int& v : a[u])
-
-    {
-        if (v == p)
-            continue;
-        dfs(v);
-        paths.push_back(u);
-    }
 }
