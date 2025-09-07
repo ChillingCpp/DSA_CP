e2536b15463656a0817dc0d56582c172dbd1235f
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-09-07 21:08:19 +0700

    Update sieve_and_variations.cpp

--- Algorithms/math/sieve_and_variations.cpp
+++ Algorithms/math/sieve_and_variations.cpp
@@ -1,26 +1,12 @@
-// template for some problems using sieve, mean try to do something with 2n, 3n, 4n,... with n is prime or not
+// template for some problems using sieve, O(n log n)
 void sieve_template(){
 
     int n = 1e6;
     vector<int> sieve(n+1, 1);
-    sieve[0] = sieve[1] = 0;
-    for (int i = 2; i *i <= n; ++i)
+    for (int i = 2; i <= n; ++i)
     {
-        if (sieve[i])
-        for (int j = i * 2; j <= n; j += i)
-            // compute something with i, 2i, 3i, 4i,... 
-    }
-}
-
-int n = 1e6; // can up to 1e7 - 1e8
-vector<int> sieve(n + 1, 1);
-void normal_sieve(){
-    sieve[0] = sieve[1] = 0;
-    for (int i = 2; i * i <= n; ++i)
-    {
-        if (sieve[i]) 
-        for (int j = i * i; j <= n; j += i) 
-            sieve[j] = 0;
+        for (int j = i; j <= n; j += i)
+            // compute something with i, 2i, 3i, 4i,... , like the f(x) function 
     }
 }
 void segment_sieve(ll l, ll r)
@@ -29,6 +15,8 @@ void segment_sieve(ll l, ll r)
     /// sieve 1 to sqrt(r)
     vector<bool> mark(sqr + 1, 1);
     vector<ll> sieve(r - l + 1, true), primes, pri;
+    prime.reserve(r - l + 1);
+    pri.reserve(r - l + 1);
     for (int i = 2; i <= sqr; ++i)
     {
         if (mark[i]){
@@ -50,31 +38,57 @@ void segment_sieve(ll l, ll r)
 }
 
 
-// smallest prime factor, help factoring a number in O(log n)
-int n = 1e6; // can up to 1e7 - 1e8
-vector<int> spf(n + 1, 0);
-void compute_spf(){
+//// all in one sieve from [1...n]
+int                 n = 1e6 + 2;
+vector<int>         prime(n, 1), divcnt(n, 1), divsum(n, 1), f(n), phi(n), spf(n), bpf(n);
+vector<vector<int>> divlist(n, { 1 });
+
+void sieve()
+{
+    divsum[0] = prime[0] = prime[1] = 0;
+
+    divcnt[0] = 0;
+    f[0] = f[1] = 1;  /// compute somthing about f(x)
+    divlist[0]  = {};
+
     iota(spf.begin(), spf.end(), 0);
+    iota(bpf.begin(), bpf.end(), 0);
+    iota(phi.begin(), phi.end(), 0);
+
+    // i* i <= n only for prime sieve
+    /// O(n log log n)
     for (int i = 2; i * i <= n; ++i)
     {
-        if (spf[i] == i) 
-        for (int j = i * i; j <= n; j += i) 
-            if (spf[j] == j) 
-                spf[j] = i;
+        if (prime[i])
+            for (int j = i * i; j <= n; j += i)
+                prime[j] = 0;
     }
-}
-// euler totient
-int n = 1e6; // can up to 1e7 - 1e8
-vector<int> phi(n + 1, 0);
-void        compute_phi()
-{
-    iota(phi.begin(), phi.end(), 0);
+
+    /// O(n log log n)
     for (int i = 2; i <= n; ++i)
     {
-        if (phi[i] == i)
+        if (spf[i] == i)
+            for (int j = i; j <= n; j += i)
+            {
+                if (spf[j] == j) spf[j] = i;
+                bpf[j] = i;
+                phi[j] -= phi[j] / i;
+            }
+    }
+    /// O(n log n)
+    for (int i = 2; i <= n; ++i)
+    {
+        f[i] = ...;  /// calculate f[i]
         for (int j = i; j <= n; j += i)
-            phi[j] -= phi[j] / i;  // phi[j] *= (i - 1) / i;
+        {
+            if (i != j){
+                f[j] = ...;  /// calc f[2*i], f[3*i].... base on f[i]
+                f[i] = ...;  /// or vice versa
+            }
+
+            divcnt[j]++;
+            divsum[j] += i;
+            divlist[j].push_back(i);
+        }
     }
 }
-
-
