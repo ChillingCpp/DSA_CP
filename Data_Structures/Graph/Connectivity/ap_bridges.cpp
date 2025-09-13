c90ab950be039d701e949af68b8aef9daeb17940
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-09-13 21:47:42 +0700

    Update ap_bridges.cpp

--- Data_Structures/Graph/Connectivity/ap_bridges.cpp
+++ Data_Structures/Graph/Connectivity/ap_bridges.cpp
@@ -1,49 +1,66 @@
-struct Node
+struct Tarjan
 {
-    int low = 0, disc = 0, ap = 0;
-};
-struct Bridge
-{
-    int u = 0, v = 0;
-};
-vvi            a, sccs;
-vector<Node>   node;
-vector<Bridge> bridge;
-
-int  t = 0;
-void tarjan(int u, int p)
-{
-    node[u].low = node[u].disc = ++t;
-
-    int child = 0;
-
-    for (int v : a[u])
+    struct Node
     {
-        if (v == p)
-            continue;
+        int low = 0, disc = 0, ap = 0;
+    };
+    struct Bridge
+    {
+        int u = 0, v = 0;
+    };
+    vvi            a, sccs;
+    vector<Node>   vt;
+    vector<Bridge> bridge;
+    int            n, t = 0;
 
-        if (!node[v].disc)
-        {
-            child++;
-            tarjan(v, u);
-            /// if the v is in the component of u or v is discovery earlier than u then u is the
-            /// articular point
-            if (node[u].disc <= node[v].low)
-                node[u].ap = 1;
-            // if (disc[v] == low[u]) then we have encounterd a cycle, if we disconnected the edge
-            // {u, v} then nothing will happened
-            if (node[u].disc < node[v].low)  //
-                bridge.push_back({ u, v });
-
-            node[u].low = min(node[u].low, node[v].low);
-        }
-        else
-            node[u].low = min(node[u].low, node[v].disc);
+    void init(int _n)
+    {
+        n = _n;
+        a.resize(n);
+        vt.resize(n);
     }
-    if (child > 1)
-        node[u].ap = 1;
-}
-// main code //
-for (int i = 1; i <= n; ++i)
-    if (!node[i].disc)
-        tarjan(i);
+    void ae(int u, int v)
+    {
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+    void tarjan(int u, int p)
+    {
+        vt[u].low = vt[u].disc = ++t;
+
+        int child = 0;
+
+        for (int v : a[u])
+        {
+            if (v == p)
+                continue;
+
+            if (!vt[v].disc)
+            {
+                child++;
+                tarjan(v, u);
+                /// if the v is in the component of u or v is discovery earlier than u then u is the
+                /// articular point
+                if (vt[u].disc <= vt[v].low)
+                    vt[u].ap = 1;
+                // if (disc[v] == low[u]) then we have encounterd a cycle, if we disconnected the
+                // edge {u, v} then nothing will happened
+                if (vt[u].disc < vt[v].low)  //
+                    bridge.push_back({ u, v });
+
+                vt[u].low = min(vt[u].low, vt[v].low);
+            }
+            else
+                vt[u].low = min(vt[u].low, vt[v].disc);
+        }
+        if (child > 1)
+            vt[u].ap = 1;
+    }
+    // main code //
+    void process()
+    {
+        for (int i = 1; i <= n; ++i)
+            if (!vt[i].disc)
+                tarjan(i, i);
+    }
+};
