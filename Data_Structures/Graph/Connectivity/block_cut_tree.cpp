495736e50636282dc87ff79eadebe974c7a56dd8
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-09-14 12:13:59 +0700

    Update block_cut_tree.cpp

--- Data_Structures/Graph/Connectivity/block_cut_tree.cpp
+++ Data_Structures/Graph/Connectivity/block_cut_tree.cpp
@@ -1,77 +1,45 @@
 struct BCT
 {
-    struct Node { int disc = 0, low = 0, ap = 0; };
-    vector<Node> vt;
-    vvi          bct, comp;
-    stack<pii>   st;
-    int          n, time = 0;
-
-    void getcomp(pii ed)
-    {
-        vi c;
-        while (st.size())
-        {
-            auto e = st.top(), st.pop();
-            c.push_back(e.first), c.push_back(e.second);
-            if (e == ed) break;
-        }
-        comp.push_back(c);
-    }
+    int         n, bccs = 1, time = 0;
+    vvi         bct;
+    vector<int> st, low, disc;
 
     void build(vvi& a)
     {
         n = a.size() - 1;
-        vt.resize(n + 1);
+        bct.resize(n + 1);
         function<void(int, int)> dfs = [&](int u, int p = -1)
         {
-            vt[u].disc = vt[u].low = ++time;
-            int child              = 0;
+            low[u] = disc[u] = ++time;
+            st.push_back(u);
             for (int v : a[u])
             {
-                if (v == p) continue;
-                if (!vt[v].disc)
+                if (v == p)
+                    continue;
+                if (!disc[v])
                 {
-                    st.emplace(u, v);
-                    child++;
                     dfs(v, u);
-
-                    if (p != -1 && vt[u].disc <= vt[v].low)
+                    low[u] = min(low[u], low[v]);
+                    if (low[v] >= disc[u])
                     {
-                        vt[u].ap = 1;
-                        getcomp({ u, v }); // get component when encoutered a articulation point
+                        bct.push_back({});
+                        bct[u].push_back(n + bccs);
+                        while (st.size())
+                        {
+                            bct[n + bccs].push_back(st.back());
+                            st.pop_back();
+                            if (bct[n + bccs].back() == v)
+                                break;
+                        }
+                        bccs++;
                     }
-                    vt[u].low = min(vt[u].low, vt[v].low);
                 }
                 else
-                {
-                    st.emplace(u, v);
-                    vt[u].low = min(vt[u].low, vt[v].disc);
-                }
+                    low[u] = min(low[u], disc[v]);
             }
-            if (p == -1 && child > 1) vt[u].ap = 1;
         };
         for (int i = 1; i <= n; ++i)
-        {
-            if (!vt[i].disc)
-            {
+            if (!disc[i])
                 dfs(i, -1);
-                if (st.size()) getcomp(st.top()); // get all left to a componenet
-            }
-        }
-        int cnt   = comp.size();
-        int total = n + 1 + cnt;
-        bct.resize(total + 1);
-        for (int i = 0; i < cnt; ++i)
-        {
-            int node = n + i + 1;
-            for (int v : comp[i])
-            {
-                if (vt[v].ap)
-                {
-                    bct[v].push_back(node);
-                    bct[node].push_back(v);
-                }
-            }
-        }
     }
 };
