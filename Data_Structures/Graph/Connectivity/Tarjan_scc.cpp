726d0a1884effedd3847a9c063e47b5cf5d43487
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-09-14 13:49:46 +0700

    Update Tarjan_scc.cpp

--- Data_Structures/Graph/Connectivity/Tarjan_scc.cpp
+++ Data_Structures/Graph/Connectivity/Tarjan_scc.cpp
@@ -1,55 +1,54 @@
-
-// tarjan algorithms for scc
-struct Node
+struct Tarjan
 {
-    int  low = 0, disc = 0;
-    bool st = false;
+    vvi         a, sccs;
+    vector<int> low, disc, st;
+    stack<ll>   s;
+
+    int  t = 0;
+    void dfs(int u)
+    {
+        low[u] = disc[u] = ++t;
+        st[u]            = true;
+        s.push(u);
+
+        for (auto& v : a[u])
+        {
+            if (!disc[v])
+            {
+                dfs(v);
+                /// low[u] = min(low[u], low[v]) khi v chưa được khám phá là do ta có đường đi từ u
+                /// -> v, mà ta lại biết v có thể quay về 1 tổ tiên có disc nhỏ nhất ( gọi là z ),
+                /// vậy nên u cũng có thể quay về tổ tiên z đó ( theo u -> v -> z )
+                low[u] = min(low[u], low[v]);
+            }
+            else if (st[v])
+                /// low[u] = min(low[u], disc[v]) khi v đã khám phá là do v là tổ tiên của u, xét
+                /// hiện tại thì ta chỉ biết u có thể quay về v ta sẽ không tính low[u] =
+                /// min(low[u], low[v]) vì low[u] sẽ chỉ tính các back edge quay ngược trực tiếp chứ
+                /// không phải qua 2 lần chạy back edge hoặc nhiều hơn từ u -> v -> z, với z là đỉnh
+                /// có disc[z] = low[v], v có back edge khác ta sẽ không xét
+                low[u] = min(low[u], disc[v]);
+        }
+
+        if (low[u] == disc[u])
+        {
+            vi scc;
+            while (true)
+            {
+                int v = s.top();
+                st[v] = false;
+                scc.push_back(v);
+                if (v == u)
+                    break;
+            }
+            sccs.push_back(scc);
+        }
+    }
+
+    void process()
+    {
+        for (int i = 1; i <= n; ++i)
+            if (!disc[i])
+                dfs(i);
+    }
 };
-vvi          a, sccs;
-vector<Node> node;
-stack<ll>    s;
-
-int  t = 0;
-void tarjan(int u)
-{
-    node[u].low = node[u].disc = ++t;
-    s.push(u);
-    node[u].st = true;
-
-    for (auto& v : a[u])
-    {
-        if (!node[v].disc)
-        {
-            tarjan(v);
-            /// low[u] = min(low[u], low[v]) khi v chưa được khám phá là do ta có đường đi từ u -> v, 
-            /// mà ta lại biết v có thể quay về 1 tổ tiên có disc nhỏ nhất ( gọi là z ), 
-            /// vậy nên u cũng có thể quay về tổ tiên z đó ( theo u -> v -> z )
-            node[u].low = min(node[u].low, node[v].low);
-        }
-        else if (node[v].st)
-            /// low[u] = min(low[u], disc[v]) khi v đã khám phá là do v là tổ tiên của u, xét hiện tại thì ta chỉ biết u có thể quay về v
-            /// ta sẽ không tính low[u] = min(low[u], low[v]) vì low[u] sẽ chỉ tính các back edge quay ngược trực tiếp 
-            /// chứ không phải qua 2 lần chạy back edge hoặc nhiều hơn từ u -> v -> z, với z là đỉnh có disc[z] = low[v], v có back edge khác ta sẽ không xét
-            node[u].low = min(node[u].low, node[v].disc);
-    }
-
-    if (node[u].low == node[u].disc)
-    {
-        vi scc;
-        while (true)
-        {
-            int v = s.top();
-            s.pop();
-            node[v].st = false;
-            scc.push_back(v);
-            if (v == u)
-                break;
-        }
-        sccs.push_back(scc);
-    }
-}
-// main code //
-for (int i = 1; i <= n; ++i)
-    if (!node[i].disc)
-        tarjan(i);
-
