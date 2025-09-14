33ad0a3af1ce71d0b8394778e42104cf44fc5040
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-09-14 12:19:42 +0700

    Update ap_bridges.cpp

--- Data_Structures/Graph/Connectivity/ap_bridges.cpp
+++ Data_Structures/Graph/Connectivity/ap_bridges.cpp
@@ -1,66 +1,53 @@
 struct Tarjan
 {
-    struct Node
-    {
-        int low = 0, disc = 0, ap = 0;
-    };
     struct Bridge
     {
         int u = 0, v = 0;
     };
     vvi            a, sccs;
-    vector<Node>   vt;
+    vector<int>    disc, low, ap;
     vector<Bridge> bridge;
     int            n, t = 0;
 
     void init(int _n)
     {
         n = _n;
-        a.resize(n);
-        vt.resize(n);
+        a.resize(n + 1);
+        disc.resize(n + 1);
+        low.resize(n + 1);
+        ap.resize(n + 1);
     }
     void ae(int u, int v)
     {
         a[u].push_back(v);
         a[v].push_back(u);
     }
-    void tarjan(int u, int p)
+    void dfs(int u, int p = -1)
     {
-        vt[u].low = vt[u].disc = ++t;
-
-        int child = 0;
-
+        low[u] = disc[u] = ++t;
+        int child        = 0;
         for (int v : a[u])
         {
             if (v == p)
                 continue;
 
-            if (!vt[v].disc)
+            if (!disc[v])
             {
                 child++;
-                tarjan(v, u);
-                /// if the v is in the component of u or v is discovery earlier than u then u is the
-                /// articular point
-                if (p != -1 && vt[u].disc <= vt[v].low)
-                    vt[u].ap = 1;
-                // if (disc[v] == low[u]) then we have encounterd a cycle, if we disconnected the
-                // edge {u, v} then nothing will happened
-                if (vt[u].disc < vt[v].low)  //
-                    bridge.push_back({ u, v });
-
-                vt[u].low = min(vt[u].low, vt[v].low);
+                dfs(v, u);
+                if (p != -1 && disc[u] <= low[v])  ap[u] = 1;
+                if (disc[u] < low[v])  bridge.push_back({ u, v });
+                low[u] = min(low[u], low[v]);
             }
             else
-                vt[u].low = min(vt[u].low, vt[v].disc);
+                low[u] = min(low[u], disc[v]);
         }
-        if (p == -1 && child > 1)
-            vt[u].ap = 1;
+        if (p == -1 && child > 1)  ap[u] = 1;
     }
     // main code //
     void process()
     {
         for (int i = 1; i <= n; ++i)
-            if (!vt[i].disc)
-                tarjan(i, i);
+            if (!disc[i]) dfs(i);
     }
 };
