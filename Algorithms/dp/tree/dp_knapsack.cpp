5cd2b70c9ed4448836dac83a0bb96e3978b7de4e
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-09-16 10:25:16 +0700

    Update dp_knapsack.cpp

--- Algorithms/dp/tree/dp_knapsack.cpp
+++ Algorithms/dp/tree/dp_knapsack.cpp
@@ -1,1 +1,15 @@
+void dfs(int u, int p = 0){
 
+    dp[u][wt[u]] = val[u];
+    for (int v : a[u])
+    if (v != p){
+        dfs(v, u);
+
+        vi new_dp = dp[u];
+        for (int i = 0; i <= W; ++i)
+            for (int j = 0; j <= W - i; ++j)
+                new_dp[i+j] = max(new_dp[i+j], dp[u][i] + dp[v][j]); // using i weight in dp[u] and j weight in dp[v]
+
+        dp[u] = new_dp;
+    }
+}
