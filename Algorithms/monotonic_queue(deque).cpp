660fbafa369252eedd645129e6cc75425d0f23bf
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-09-16 15:10:37 +0700

    Update monotonic_queue(deque).cpp

--- Algorithms/monotonic_queue(deque).cpp
+++ Algorithms/monotonic_queue(deque).cpp
@@ -1,1 +1,20 @@
+vi sliding(vector<int>& a, int k, function<ll(ll, ll)> cmp)
+{
+    int n = a.size();
+    deque<int>dq;
+    vi ans;
+    for (int i = 0; i < k; ++i){
 
+        while (dq.size() && cmp(a[dq.back()], a[i])) dq.pop_back();
+        dq.push_back(i);
+    }
+    ans.push_back(a[dq.front()]);
+    for (int i = k; i < n; ++i)
+    {
+        while (dq.size() && dq.front() < i - k + 1) dq.pop_front();
+        while (dq.size() && cmp(a[dq.back()], a[i])) dq.pop_back();
+        ans.push_back(a[dq.front()]);
+        dq.push_back(i);
+    }
+    return ans;
+}
