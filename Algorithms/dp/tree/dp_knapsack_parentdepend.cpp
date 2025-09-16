5c71ed569dc62ff1d52e9002e9f6e5b343c660ca
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-09-16 10:27:07 +0700

    Update dp_knapsack_parentdepend.cpp

--- Algorithms/dp/tree/dp_knapsack_parentdepend.cpp
+++ Algorithms/dp/tree/dp_knapsack_parentdepend.cpp
@@ -1,6 +1,4 @@
-// the kind of problem is 
 /// You are given a rooted tree where each node has a weight and a profit. You want to select a subset of nodes such that:
-
 /// The parent of a selected node must also be selected. /// this is when we can optimization
 /// The total weight does not exceed W.
 /// The goal is to maximize total profit.
@@ -28,30 +26,4 @@ void dfs2(int u)
             kn[p[u]][i+ wt[u]] = max(kn[p[u]][i], pr[u] + kn[u][i]);
         }
     }
-}
-int main()
-{
-    // assign knapsack to -1, kn[0][0] = 0;
-    cin >> n >> W;
-
-    kn.assign(n+1, vi(W+1, -1));
-    kn[0][0] = 0;
-    p.resize(n+1);
-    a.resize(n+1);
-    wt.resize(n+1);
-    pr.resize(n+1);
-
-    for (int i = 1; i <= n; ++i)
-        cin >> wt[i] >> pr[i];
-
-    for (int i= 0; i < n-1; ++i){
-        int u, v;
-        cin >> u >> v;
-        a[v].push_back(u);
-        a[u].push_back(v);
-    }
-    dfs1(1); /// rooted at 1
-    dfs2(1); // dfs
-
-    cout << *std::max_element(kn[0].begin(), kn[0].end()) << '\n';
 }
