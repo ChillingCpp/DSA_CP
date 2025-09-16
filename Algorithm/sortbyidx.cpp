dfbb609be8b13e60af327e38e08b04cb66002875
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-09-16 14:51:28 +0700

    Update sortbyidx.cpp

--- Algorithm/sortbyidx.cpp
+++ Algorithm/sortbyidx.cpp
@@ -9,4 +9,11 @@ sort(idx.begin(), idx.end(), [&}(int&p, int&q){return a[p] < a[q]; });
 /// all element mark as -1 is unactive, you cannot process it more, find the minimum number of operations to make all element unactive
 
 int minop = 0;
-int l = 0, r = 0;
+int cur = idx[0];
+for (int i = 1; i < n; ++i)
+{
+    if (a[idx[i]] > a[cur] || (idx[i] - cur +1) > K){
+        cur = idx[i];
+        minop++;
+    }
+}
