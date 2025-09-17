af919f7e8f121696ad295ea8eb78e9dbb663a3a0
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-09-17 23:17:08 +0700

    Update hilbert_order.cpp

--- range_query/sqrt_decomposition/MO_algorithm/hilbert_order.cpp
+++ range_query/sqrt_decomposition/MO_algorithm/hilbert_order.cpp
@@ -15,18 +15,13 @@ inline ll hilbert_order(int x, int y, int pow)
         int nx = x & (hp - 1), ny = y & (hp - 1);
         
         res += seg * (1ll << (2 * p - 2));
-        switch (seg)
-        {
-            case 0:
-                x = ny, y = nx;
-                break;
-            case 3:
-                x = hp - ny - 1, y = hp - nx - 1;
-                break;
-            default:
-                x = nx, y = ny;
-                break;
-        }
+        if (seg == 0)
+             x = ny, y = nx;
+        else  if (seg == 3)
+             x = hp - ny - 1, y = hp - nx - 1;
+        else 
+             x = nx, y = ny;
     }
     return res;
 }
+
