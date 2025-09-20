1a76cfdfe5220322a7bed6ec7f8fe46960402ea0
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-09-20 20:16:19 +0700

    Create Splitting_on_demands.cpp

---
+++ Algorithms/Greedy/Splitting_on_demands.cpp
@@ -0,0 +1,57 @@
+// examples : CF 309C
+
+int main()
+{
+    fastio;
+
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    vector<int> freqa(32), freqb(32);
+    for (auto& i : a)
+    {
+        cin >> i;
+        for (int j = 30; j >= 0; --j)
+            if ((i >> j) & 1)
+                freqa[j]++;
+    }
+    for (auto& i : b)
+    {
+        cin >> i;
+        freqb[i]++;
+    }
+
+    int cnt = 0;
+
+    for (int iter = 0; iter <= 1; ++iter)
+        for (int i = 0; i <= 30; ++i)
+            if (freqa[i] > freqb[i])
+                for (int j = i - 1; j >= 0 && freqa[i] > freqb[i]; --j)
+                {
+                    if (freqa[j] >= freqb[j])
+                        continue;
+                    int diff = freqb[j] - freqa[j];
+                    int val  = (1 << (i - j));
+                    int num  = (diff + val - 1) / val;
+                    diff -= min(num, freqa[i]) * val;
+                    freqa[j] += min(num, freqa[i]) * val;
+                    freqa[i] -= min(num, freqa[i]);
+                }
+    for (int iter = 0; iter <= 1; ++iter)
+        for (int i = 0; i <= 30; ++i)
+            for (int j = i - 1; j >= 0; --j)
+            {
+                if (freqa[j] >= freqb[j])
+                    continue;
+                int diff = freqb[j] - freqa[j];
+                int val  = (1 << (i - j));
+                int num  = (diff + val - 1) / val;
+                diff -= min(num, freqa[i]) * val;
+                freqa[j] += min(num, freqa[i]) * val;
+                freqa[i] -= min(num, freqa[i]);
+            }
+
+    for (int i = 0; i <= 30; ++i)
+        cnt += min(freqa[i], freqb[i]);
+    cout << cnt << '\n';
+}
