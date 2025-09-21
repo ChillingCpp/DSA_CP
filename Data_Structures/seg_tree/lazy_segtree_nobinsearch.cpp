b7ff77319551387e131f363c6f38ae02d8182bc3
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-09-21 22:11:45 +0700

    Update lazy_segtree_nobinsearch.cpp

--- Data_Structures/seg_tree/lazy_segtree_nobinsearch.cpp
+++ Data_Structures/seg_tree/lazy_segtree_nobinsearch.cpp
@@ -7,7 +7,6 @@ struct Node
         , len(sz)
         {}
     };
-
     struct Lazy
     {
         bool      has_set;
@@ -17,17 +16,14 @@ struct Node
         , val(v)
         {}
     };
-
     Node op(Node a, Node b)
     {
         return Node(a.sum + b.sum, a.len + b.len);
     }
-
-    Node id_node()
+    Node in()
     {
         return {0, 0};
     }
-
     Node transfer(Lazy f, Node node)
     {
         if (node.len == 0)
@@ -36,9 +32,8 @@ struct Node
             return Node(f.val * node.len, node.len);
         return Node(node.sum + f.val * node.len, node.len);
     }
-
     /// nw nghĩa là lazy hiện tại, prev là lazy trước đó, hàm op này kết hợp nw và prev
-    Lazy op_lazy(Lazy nw, Lazy prev)
+    Lazy op_l(Lazy nw, Lazy prev)
     {
         if (nw.has_set)
             return nw;
@@ -50,149 +45,108 @@ struct Node
                 return Lazy(false, prev.val + nw.val);
         }
     }
-    Lazy id_lazy()
+    Lazy idl()
     {
         return {0, 0};
     }
-
-struct lazy_segtree
+struct Seg
 {
-public:
-    lazy_segtree()
-    : lazy_segtree(0)
-    {
-    }
-    explicit lazy_segtree(int n)
-    : lazy_segtree(std::vector<Node>(n, id_node()))
-    {
-    }
-    explicit lazy_segtree(const std::vector<Node>& v)
-    : _n(int(v.size()))
-    {
-        size = (int) bit_ceil((unsigned int) (_n));
-        log  = __builtin_ctz(size);
-        d    = std::vector<Node>(2 * size, id_node());
-        lz   = std::vector<Lazy>(size, id_lazy());
-        std::copy(v.begin(), v.end(), d.begin() + size);
+    vector<Node> st;
+    vector<Lazy> lazy;
+    int          n, h;
 
-        for (int i = size - 1; i >= 1; i--)
+    Seg() : Seg(0)
+    Seg(int n)
+    : Seg(vector<Node>(n)) {}
+    Seg(const vector<Node>& a)
+    {
+        int sz = a.size();
+        n = 1, h = 0;
+        
+        while (n < sz)  {  n <<= 1,  h++;  }
+        
+        st.resize(2 * n);
+        lazy.resize(n);
+        copy(a.begin(), a.end(), st.begin() + n);
+        for (int i = n - 1; i > 0; --i)
             update(i);
     }
-
-    void set(int p, Node x)
-    {
-        p += size;
-
-        push(p, p + 1);
-        d[p] = x;  // assign
-        update(p, p + 1);
-    }
-    
-    Node query(int l, int r)
-    {
-        if (l == r)
-            return id_node();
-
-        l += size;
-        r += size;
-        push(l, r);
-
-        Node ans = id_node();
-        for (; l < r; l >>= 1, r >>= 1)
-        {
-            if (l & 1)
-                ans = op(ans, d[l++]);
-            if (r & 1)
-                ans = op(ans, d[--r]);
-        }
-        return ans;
-    }
-
     Node all_query()
     {
         return d[1];
     }
-
-    void apply(int l, int r, Lazy f)
+    Node query(int l, int r)
     {
-        if (l == r)
-            return;
-
-        l += size;
-        r += size;
-
+        l += n, r += n;
         push(l, r);
-
-        for (int l2 = l, r2 = r; l2 < r2; l2 >>= 1, r2 >>= 1)
+        Node res = idn();
+        for (; l < r; l >>= 1, r >>= 1)
         {
-            if (l2 & 1)
-                all_apply(l2++, f);
-            if (r2 & 1)
-                all_apply(--r2, f);
+            if (l & 1)  res = op(res, st[l++]);
+            if (r & 1)  res = op(res, st[--r]);
         }
-
-        update(l, r);
+        return res;
     }
-private:
-    int               _n, size, log;
-    std::vector<Node> d;
-    std::vector<Lazy> lz;
-
-    void all_apply(int k, Lazy f)
+    void apply(int l, int r, Lazy val)
     {
-        d[k] = transfer(f, d[k]);  /// apply to node
-        if (k < size)              /// if still in parent then propagate lazy
-            lz[k] = op_lazy(f, lz[k]);
+        l += n, r += n;
+        push(l, r);
+        int  l1 = l, r1 = r;
+        for (; l < r; l >>= 1, r >>= 1)
+        {
+            if (l & 1)  apply_all(l++, val);
+            if (r & 1)  apply_all(--r, val);
+        }
+        update(l1, r1);
+    }
+    void apply_all(int p, Lazy val)
+    {
+        st[p] = transfer(st[p], val);
+        if (p < n) lazy[p] = op_l(lazy[p], val);
+    }
+    void push(int p)
+    {
+        apply_all(p << 1, lazy[p]);
+        apply_all(p << 1 | 1, lazy[p]);
+        lazy[p] = idl();
+    }
+    void update(int p)
+    {
+        st[p] = op(st[p << 1], st[p << 1 | 1]);
     }
     void push(int l, int r)
     {
-        for (int i = log; i >= 1; i--)
+        for (int i = h; i >= 1; --i)
         {
-            if (((l >> i) << i) != l)
-                push(l >> i);
-            if (((r >> i) << i) != r)
-                push((r - 1) >> i);
+            if (((l >> i) << i) != l)  push(l >> i);
+            if (((r >> i) << i) != r)  push((r - 1) >> i);
         }
     }
     void update(int l, int r)
     {
-        for (int i = 1; i <= log; i++) /// update range [l, r)
+        for (int i = 1; i <= h; ++i)
         {
-            if (((l >> i) << i) != l)
-                update(l >> i);
-            if (((r >> i) << i) != r)
-                update((r - 1) >> i);
-        }   
-    }
-
-    void update(int k)
-    {
-        d[k] = op(d[2 * k], d[2 * k + 1]);  /// merge
-    }
-    void push(int k)
-    {
-        all_apply(2 * k, lz[k]);
-        all_apply(2 * k + 1, lz[k]);
-        lz[k] = id_lazy();
-    }
-    int bit_ceil(unsigned x)
-    {
-        if (x <= 1)
-            return 1;
-        return 1u << (32 - __builtin_clz(x - 1));
+            if (((l >> i) << i) != l)  update(l >> i);
+            if (((r >> i) << i) != r)  update((r - 1) >> i);
+        }
     }
 };
-
 /// các hàm này có thể thêm vào
+void set(int p, Node x)
+{
+    p += size;
+
+    push(p);
+    d[p] = x;  // assign
+    update(p);
+}
 void pushp(int p)
 {
-    for (int i = log; i >= 1; i--)
-        push(p >> i);
+    for (int i = log; i >= 1; i--)  push(p >> i);
 }
 void updatep(int p)
 {
-    for (int i = 1; i <= log; i++)
-        update(p >> i);
+    for (int i = 1; i <= log; i++) update(p >> i);
 }
 
 Node getDebug(int p)
