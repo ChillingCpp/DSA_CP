15da1546a1193350ab08d90cf278ddc32dadef7a
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-10-18 08:51:47 +0700

    Update manacher.cpp

--- Algorithms/manacher.cpp
+++ Algorithms/manacher.cpp
@@ -7,7 +7,7 @@ vector<int> manacher_odd(string& s)
     for (int i = 1; i <= n; ++i)
     {
         p[i] = min(r - i, p[l + r - i]);
-        while (s[i + p[i]] == s[i - p[i]])
+        while (i + p[i] <= n + 1 && i - p[i] >= 0 && s[i + p[i]] == s[i - p[i]])
             p[i]++;
         if (i + p[i] > r)
         {
@@ -17,18 +17,20 @@ vector<int> manacher_odd(string& s)
     }
     return vector<int>(p.begin() + 1, p.end() - 1);
 }
-vector<int> manacher(string& s)
+int manacher(string& s)
 {
     int n = s.size();
 
-    vector<int> ans(n);
+    int ans = 0;
     string      t;
-    for (auto c : s)
-        t += string('#', 1) + c;
+    for (auto c : s){
+        t += '#';
+        t += c;
+    }
     t += '#';
     vector<int> p = manacher_odd(t);
     for (int i = 0; i < n; ++i)
-        ans[i] = max(p[2 * i + 1] / 2, (p[2 * i] - 1) / 2);
+        ans = max({ans, p[2 * i + 1], p[2 * i] - 1});
 
     return ans;
 }
