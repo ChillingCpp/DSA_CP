b591ca0400b3486da6d84c0ca1093d3d22668b75
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-11-08 23:21:56 +0700

    Update Binary_Search.cpp

--- Algorithms/Searching/Binary_Search.cpp
+++ Algorithms/Searching/Binary_Search.cpp
@@ -1,1 +1,28 @@
+/// binary search searching elements :
+/// requires : sorted array
+upper_bound(x) : leftmost smallest element in array > x
+lower_bound(x) : leftmost largest element in array >= x
 
+
+
+
+
+
+
+
+/// Binary search on answers
+/// requires : sorted array or check function is monotone
+bool check(int mid)
+{
+  
+}
+ll low = 0, high = 1e10;
+while (low <= high)
+{
+    ll mid = (low + high) / 2;
+    if (check(mid))
+        high = mid - 1;
+    else
+        low = mid + 1;
+}
+cout << low << '\n';
