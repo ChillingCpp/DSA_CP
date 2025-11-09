7e9e69f4d2e60ed529fcc6257dec6235d967a9b2
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2025-11-09 21:35:14 +0700

    Update Binary_Search.cpp

--- Algorithms/Searching/Binary_Search.cpp
+++ Algorithms/Searching/Binary_Search.cpp
@@ -3,14 +3,7 @@
 upper_bound(x) : leftmost smallest element in array > x
 lower_bound(x) : leftmost largest element in array >= x
 
-
-
-
-
-
-
-
-/// Binary search on answers
+/// Binary search on answers, integer range
 /// requires : sorted array or check function is monotone
 bool check(int mid)
 {
@@ -21,8 +14,26 @@ while (low <= high)
 {
     ll mid = (low + high) / 2;
     if (check(mid))
-        high = mid - 1;
+        high = mid - 1; // or  low= mid + 1
     else
-        low = mid + 1;
+        low = mid + 1; // or high = mid -1;
+}
+cout << low << '\n';
+
+/// Binary search on answers, real value range [L, R]
+/// requires : sorted array or check function is monotone
+bool check(double mid)
+{
+  
+}
+double low = 0, high = 1e10, eps = 1e-7;
+int iter = int(log2((r - l)/ eps))+ 3;
+while (iter--)
+{
+    ll mid = (low + high) / 2;
+    if (check(mid))
+        low = mid;
+    else
+        high = mid;
 }
 cout << low << '\n';
