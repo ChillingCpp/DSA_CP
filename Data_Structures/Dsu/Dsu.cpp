1d703b7353a41e24f8342dc9dce20d52d9883e33
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2026-01-18 08:30:49 +0700

    Update Dsu.cpp

--- Data_Structures/Dsu/Dsu.cpp
+++ Data_Structures/Dsu/Dsu.cpp
@@ -1,25 +1,39 @@
 struct Dsu
 {
-    vector<int> parent;
+    vector<int> p;
     vector<int> sz;
+    // nxt[i] được định nghĩa là vị trí kế tiếp về bên phải chưa được xử lí
+    // ban đầu nxt[i] = i do i chưa được xử lí
+    vector<int> nxt;  // jump pointer, this version now only support linear
     int         compos;
 
     Dsu(int n)
-    : parent(n)
+    : p(n)
     , sz(n, 1)
     , compos(n)
+    , nxt(n + 1)
     {
-        iota(parent.begin(), parent.end(), 0);
+        iota(p.begin(), p.end(), 0);
+        iota(nxt.begin(), nxt.end(), 0);
     }
     int find(int x)
     {
         /// path compression + union by size : O(α(n))
-        if (parent[x] != x) parent[x] = find(parent[x]);
-        return parent[x];
+        if (x == p[x])
+            return x;
+        return p[x] = find(p[x]);
 
         /// non compression ( O(log n) because use union by size )
-        for (; x != parent[x]; x = parent[x]){}
+        for (; x != p[x]; x = p[x])
+        {
+        }
         return x;
+    }
+    int findnxt(int x)
+    {
+        if (x == nxt[x])
+            return x;
+        return nxt[x] = find(nxt[x]);
     }
     bool unite(int x, int y)
     {
@@ -28,18 +42,20 @@ struct Dsu
         if (rx == ry)
             return false;
 
-        if (sz[rx] < sz[ry])
-        {
-            parent[rx] = ry;
-            sz[ry] += sz[rx];
-        }
-        else
-        {
-            parent[ry] = rx;
-            sz[rx] += sz[ry];
-        }
-        compos--;
+        if (sz[x] < sz[y])
+            swap(x, y);
+        p[y] = x;
+        sz[x] += sz[y];
         return true;
+    }
+    /// nxt pointer support subarray query
+    void urange(int l, int r)
+    {
+        for (int v = findnxt(l); v <= r; v = findnxt(v))
+        {
+            /// do something
+            p[v] = v + 1;
+        }
     }
     // get total size of a component
     int getSize(int x)
