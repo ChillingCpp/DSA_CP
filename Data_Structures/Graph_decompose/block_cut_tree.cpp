422857fe8521fc5fde6914ec6ee6ef55b221fc69
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2026-02-16 20:53:47 +0700

    Update block_cut_tree.cpp

--- Data_Structures/Graph_decompose/block_cut_tree.cpp
+++ Data_Structures/Graph_decompose/block_cut_tree.cpp
@@ -1,47 +1,49 @@
-struct BCT
-{
-    int         n, bccs = 1, time = 0;
-    vvi         bct;
-    vector<int> st, low, disc;
-
-    void build(vvi& a)
+    struct BCT
     {
-        n = a.size() - 1;
-        bct.resize(n + 1);
-        function<void(int, int)> dfs = [&](int u, int p = -1)
+        int         n, bccs = 1, time = 0;
+        vvi         bct;
+        vector<int> st, low, disc;
+
+        void build(vvi& a)
         {
-            low[u] = disc[u] = ++time;
-            st.push_back(u);
-            for (int v : a[u])
+            n = a.size() - 1;
+            bct.resize(n + 1);
+            low.resize(n + 1);
+            disc.resize(n + 1);
+            function<void(int, int)> dfs = [&](int u, int p = -1)
             {
-                if (v == p)  
-                    continue;
-                if (!disc[v])
-                {
-                    dfs(v, u);
-                    low[u] = min(low[u], low[v]);
-                    if (low[v] >= disc[u])
+                low[u] = disc[u] = ++time;
+                st.push_back(u);
+                for (int v : a[u])
+                    if (v != p)
                     {
-                        bct.emplace_back({});
-                        bct[u].push_back(n + bccs);
-                        bct[n + bccs].push_back(u);
-                        while (st.size())
+                        if (!disc[v])
                         {
-                            int x = st.back();
-                            st.pop_back();
-                            bct[x].push_back(n + bccs);
-                            bct[n + bccs].push_back(x);
-                            if (x == v) break;
+                            dfs(v, u);
+                            low[u] = min(low[u], low[v]);
+                            if (low[v] >= disc[u])
+                            {
+                                bct.emplace_back();
+                                bct[u].push_back(n + bccs);
+                                bct[n + bccs].push_back(u);
+                                while (st.size())
+                                {
+                                    int x = st.back();
+                                    st.pop_back();
+                                    bct[x].push_back(n + bccs);
+                                    bct[n + bccs].push_back(x);
+                                    if (x == v)
+                                        break;
+                                }
+                                bccs++;
+                            }
                         }
-                        bccs++;
+                        else
+                            low[u] = min(low[u], disc[v]);
                     }
-                }
-                else
-                    low[u] = min(low[u], disc[v]);
-            }
-        };
-        for (int i = 1; i <= n; ++i)
-            if (!disc[i])
-                dfs(i, -1);
-    }
-};
+            };
+            for (int i = 1; i <= n; ++i)
+                if (!disc[i])
+                    dfs(i, -1);
+        }
+    };
