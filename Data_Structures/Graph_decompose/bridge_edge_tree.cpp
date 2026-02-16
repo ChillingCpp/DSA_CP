84b276de36325ad19e9d06d8ce8fd46290966416
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2026-02-16 16:36:43 +0700

    Update bridge_edge_tree.cpp

--- Data_Structures/Graph_decompose/bridge_edge_tree.cpp
+++ Data_Structures/Graph_decompose/bridge_edge_tree.cpp
@@ -1,54 +1,51 @@
-struct BET
-{
-    vector<int> disc, low, ins, id;
-    stack<int>  st;
-    vvi bet;
-    int n, time = 0, cmp = 0;
-    /// edge : {to, id}. id is when we get input
-    void build(vvi& a)
+    struct BET
     {
-        n = a.size()-1;
-        disc.resize(n + 1), low.resize(n + 1);
-        ins.resize(n + 1), id.resize(n + 1);
-        function<void(int, int)> dfs = [&](int u, int p = -1)
+        vi   disc, low, bg, id;
+        vvi  bet;
+        int  t = 0, cmp = 1;
+        void dfs(int u, int pid = -1)
         {
-            disc[u] = low[u] = ++time;
-            ins[u]           = 1;
-            st.push(u);
-            for (auto v : a[u])
-            {
-                if (v == p) continue;
-                if (!disc[v])
+            disc[u] = low[u] = ++t;
+            for (auto [v, id] : a[u])
+                if (id != pid)
                 {
-                    dfs(v, u);
-                    low[u] = min(low[u], low[v]);
+                    if (!disc[v])
+                    {
+                        dfs(v, id);
+                        low[u] = min(low[u], low[v]);
+                        if (low[v] > disc[u])  // nếu v không có cạnh ngược để quay về tổ tiên
+                                               // sớm hơn u thì (u, v) là cầu
+                            bg[id] = true;
+                    }
+                    else
+                        low[u] = min(low[u], disc[v]);
                 }
-                else if (ins[v])
-                    low[u] = min(low[u], disc[v]);
-            }
-            if (low[u] == disc[u])
-            {
-                cmp++;
-                while (st.size())
-                {
-                    int v = st.top();
-                    st.pop();
-                    ins[v] = 0;
-                    id[v]  = cmp;
-                    if (v == u)  break;
-                }
-            }
-        };
-        for (int i = 1; i <= n; ++i)
-            if (!disc[i])  dfs(i, -1);
+        }
+        void dfs2(int u, int pid = -1)
+        {
+            id[u] = cmp;
+            for (auto [v, i] : a[u])
+                if (!id[v] && i != pid && !bg[i])
+                    dfs2(v, i);
+        }
+        void build()
+        {
+            disc.resize(n + 1);
+            low.resize(n + 1);
+            bg.resize(m + 1);
+            id.resize(n + 1);
 
-        bet.resize(cmp + 1);
-        for (int u = 1; u <= n; ++u)
-            for (int v : a[u]) 
-                if (id[u] != id[v])
-                {
-                    bet[id[u]].push_back(id[v]);
-                    bet[id[v]].push_back(id[u]);
-                }
-    }
-};
+            for (int i = 0; i < n; ++i)
+                if (!disc[i])
+                    dfs(i, -1);
+            for (int i = 0; i < n; ++i)
+                if (!id[i])
+                    dfs2(i, -1), cmp++;
+            
+            bet.resize(cmp + 1);
+            for (int u = 1; u <= n; ++u)
+                for (auto [v, i] : a[u])
+                    if (u < v && id[u] != id[v])
+                        bet[id[u]].push_back(id[v]), bet[id[v]].push_back(id[u]);
+        }
+    };
