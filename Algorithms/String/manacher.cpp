c3a0be4d9db715b71f8e9a1c921418eb90b5b440
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2026-02-26 14:02:48 +0700

    Update manacher.cpp

--- Algorithms/String/manacher.cpp
+++ Algorithms/String/manacher.cpp
@@ -17,3 +17,38 @@ void manacherv2(string& s)
         }
     }
 }
+ll countP(vector<vector<int>> &p)
+{
+    ll ans =0;
+    for (int i =0; i < n; ++i)
+        ans += p[0][i] + p[1][0];
+    return ans;
+}
+int getmx(vector<vector<int>> &p)
+{
+    int ans = 0;
+    for (int i =0 ; i < n; ++i)
+        ans = max({ans, 2 * p[0][i], 2 *p[1][i] + 1});
+    return ans;
+}
+vector<int> LPend(vector<vector<int>> &p)
+{
+    vector<int> ans(n);
+    for (int i = 0; i < n; ++i)
+    {
+        int l0 = i - p[0][i], r0 = i + p[0][i]-1;
+        int l1 = i - p[1][i], r1 = i + p[1][i];
+        if (r0 >= l0) ans[r0] = max(ans[r0], r0 - l0 + 1);
+        ans[r1] = max(ans[r1], r1 - l1 + 1);
+    }
+    for (int i = n-1; i >= 0; --i)    
+        ans[i] = max(ans[i], ans[i+1] -2);
+    cout << ans << '\n';
+}
+bool query(int l, int r, vector<vector<int>>& p)
+{
+    int len = r - l + 1;
+    int z = len & 1;
+    int mid = (l + r + !z) /2;
+    return p[z][mid] >= len/2;
+}
