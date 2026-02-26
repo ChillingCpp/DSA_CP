e2ecbb8d8419d8a95565b267f6c12805459cd3b1
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2026-02-26 14:30:06 +0700

    Update dp_digit.cpp

--- Algorithms/dp/dp_digit.cpp
+++ Algorithms/dp/dp_digit.cpp
@@ -1,54 +1,32 @@
+T dfs(int pos, bool tight, bool started, State s) {
+    if (pos == n) return base(started, s);
 
-ll f(ll x)
-{
-  // sometime we do not return when x = 0, just in case the problem not care about number 0 
-    if (x <= 0)
-        return 0;
-    vvvi dp(2, vvi(20, vi(200)));
+    if (!tight && vis[pos][started][s]) return memo[pos][started][s];
 
-    vector<int> d;
-    while (x)
-    {
-        d.push_back(x % 10);
-        x /= 10;
-    }
-    reverse(d.begin(), d.end());
-    int len     = d.size();
-    int tot     = len * 9;
-  
-    dp[1][0][0] = 1; // tạo base case
-    /// duyệt từ chữ số hàng cao nhất tới thấp nhất
-    for (int i = 0; i < len; i++)
-    {
-        for (int sum = 0; sum <= tot; sum++)
-        {
-            for (int tight = 0; tight < 2; ++tight)
-            {
-                if (!dp[tight][i][sum])
-                    continue;
-                int lim = tight ? d[i] : 9;
-                for (int digit = 0; digit <= lim; ++digit)
-                {
-                    int nxttight = (tight && digit == lim) ? 1 : 0;
-                    dp[nxttight][i + 1][sum + digit] += dp[tight][i][sum]; // transition to state i + 1, different from compute state from previous state
-                }
-            }
-        }
+    int lim = tight ? dig[pos] : 9;
+    T ans = identity();
+
+    for (int d = 0; d <= lim; ++d) {
+        bool nstarted = started || (d != 0);
+        State ns = next_state(s, d, nstarted);
+        if (!valid_prefix(ns, nstarted)) continue;
+
+        bool ntight = tight && (d == dig[pos]);
+        T child = dfs(pos + 1, ntight, nstarted, ns);
+        ans = combine(ans, lift(child, d, pos));
     }
 
-    /// thu thập đáp án
-    ll ans = 0;
-    for (int tight = 0; tight < 2; ++tight)
-    {
-        for (int sum = 0; sum <= tot; ++sum)
-        {
-            ans += sum * dp[tight][len][sum];
-        }
+    if (!tight) {
+        vis[pos][started][s] = true;
+        memo[pos][started][s] = ans;
     }
     return ans;
 }
 
-ll g(ll l, ll r)
-{
-    return f(r) - f(l - 1);
+T solve(long long X) {
+    if (X < 0) return identity_zero_query();
+    dig = to_digits(X);
+    n = dig.size();
+    clear_memo();
+    return dfs(0, true, false, init_state());
 }
