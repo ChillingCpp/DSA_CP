4162530a42d2bfba49a539301838ec27bc67dd12
ChillingCpp <168641797+ChillingCpp@users.noreply.github.com>
2026-03-18 21:26:13 +0700

    Update seg_tree.cpp

--- Data_Structures/seg_tree/seg_tree.cpp
+++ Data_Structures/seg_tree/seg_tree.cpp
@@ -1,52 +1,42 @@
 // normal segment tree
-template<typename T>
+struct Node
+{
+};
+Node combine(Node a, Node b)
+{
+    return Node();
+}
 struct SegmentTree
 {
-public:
-    SegmentTree(vi& a)
-    : n(a.size())
-    , st(2 * a.size())
+    int          n = 1;
+    vector<Node> st;
+    SegmentTree(vector<Node>& a)
     {
+        int n1 = a.size();
+        while (n <= n1)
+            n <<= 1;
+        st.resize(2 * n);
         std::copy(a.begin(), a.end(), st.begin() + n);
         for (int i = n - 1; i > 0; --i)
-            st[i] = op(st[i << 1], st[i << 1 | 1]); 
+            st[i] = combine(st[i << 1], st[i << 1 | 1]);
     }
-
-    void add(int p, int val)
+    void set(int p, Node val)
     {
-        set(p, st[p + n] + val);
+        st[p += n] = val;
+        for (p >>= 1; p; p >>= 1)
+            st[p] = combine(st[p << 1], st[p << 1 | 1]);
     }
-    void set(int p, int val)
+    Node query(int l, int r)
     {
-        st[p + n] = val;
-        p += n;
-        for (p >>= 1; p > 1; p >>= 1)
-            st[p] = op(st[p << 1], st[p << 1 | 1]);
-    }
-    ll query(int l, int r)
-    {
-        ll res = 0;
+        Node res;
         for (l += n, r += n + 1; l < r; l >>= 1, r >>= 1)
         {
             if (l & 1)
-                res = op(st[l++], res);
+                res = combine(res, st[l++]);
             if (r & 1)
-                res = op(st[--r], res);
+                res = combine(st[--r], res);
         }
         return res;
     }
-    ll op(ll a, ll b)
-    {
-        return max(a, b);
-    }
-    // in case you want modify using op and query using op2 or vice versa.
-    ll op2(ll a, ll b)
-    {
-        return a + b;
-    }
-
-private:
-    int n;
-    vector<T>  st;
 };
 
